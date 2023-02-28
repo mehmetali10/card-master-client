279cfd56ff2f631f131cf0184c0a2f91014e109a
mehmetali10 <mhmtali2316@gmail.com>
2023-03-01 02:08:38 +0300

    add expand style

--- src/components/CreateCard.tsx
+++ src/components/CreateCard.tsx
@@ -8,13 +8,13 @@ export default function CreateCard() {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [image, setImage] = useState<string | null>(null);
+    const [isExpanded, setExpanded] = useState<boolean>(false);
 
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
       if (event.target.files && event.target.files[0]) {
         const selectedImage = URL.createObjectURL(event.target.files[0]);
         setImage(selectedImage);
-        console.log(image);
       }
     };
     const handleClick = () => {
@@ -24,55 +24,70 @@ export default function CreateCard() {
 
     function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
         setTitle(e.target.value);
-        console.log(title)
     }
 
     function handleDescriptionChange(e: React.FormEvent<HTMLTextAreaElement>) {
         setDescription(e.currentTarget.value)
     }
 
+    function expand() {
+      setExpanded(true);
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+      console.log(title);
+      console.log(description);
+      console.log(image);
+    }
+
 
     return(
         <div>
-        <form className="create-note">
+        <form className="create-note" onSubmit={handleSubmit}>
+          {isExpanded && (
             <input
               name="title"
               onChange={handleTitleChange}
               value={title}
               placeholder="Title"
             />
-  
+          )}
           <textarea
             name="content"
             onChange={handleDescriptionChange}
             value={description}
-            placeholder="Description"
-            rows={9}
+            placeholder="Description..."
+            rows={isExpanded ? 9 : 1}
+            onClick={expand}
           />
-
-        <div className="image-upload">
-              {image ? (
-                <img
-                  src={image}
-                  alt="uploaded"
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                />
-              ) : (
-                <div onClick={handleClick} className="upload-label">
-                <FileUploadIcon className="upload-icon" fontSize="large"/>
-              </div>
-            )}
-            <input
-              id="upload-input"
-              type="file"
-              accept="image/*"
-              onChange={handleChange}
-              style={{ display: "none" }}
+        {isExpanded && (
+          <div className="image-upload">
+          {image ? (
+            <img
+              src={image}
+              alt="uploaded"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
             />
-            </div>
-           <Fab >
+          ) : (
+            <div onClick={handleClick}>
+            <FileUploadIcon className="upload-icon" fontSize="large"/>
+          </div>
+        )}
+        <input
+          id="upload-input"
+          type="file"
+          accept="image/*"
+          onChange={handleChange}
+          style={{ display: "none" }}
+        />
+        </div>
+        )}
+        {isExpanded && (
+          <Fab type="submit">
             <AddIcon />
           </Fab>
+        )}
         </form>
       </div>
     )
